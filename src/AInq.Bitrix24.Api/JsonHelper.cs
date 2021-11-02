f7276d84e491c888c087dd33e3214643d8107d7e
Anton Andryushchenko <andryushchenko_anton@outlook.com>
2021-11-02 13:49:23 +0300

    [ADD] New Json helpers [FIX] Namespace [UPD] Package references

--- src/AInq.Bitrix24.Api/JsonHelper.cs
+++ src/AInq.Bitrix24.Api/JsonHelper.cs
@@ -20,7 +20,7 @@ public static class JsonHelper
     /// <summary> Try get <see cref="float" /> value </summary>
     /// <param name="token"> JSON source </param>
     public static Maybe<float> TryGetFloat(this JToken token)
-        => token.Type switch
+        => token?.Type switch
         {
             JTokenType.Float or JTokenType.Integer => Maybe.Value(token.Value<float>()),
             JTokenType.String => float.TryParse(token.Value<string>(), out var result)
@@ -38,7 +38,7 @@ public static class JsonHelper
     /// <summary> Try get <see cref="int" /> value </summary>
     /// <param name="token"> JSON source </param>
     public static Maybe<int> TryGetInt(this JToken token)
-        => token.Type switch
+        => token?.Type switch
         {
             JTokenType.Integer => Maybe.Value(token.Value<int>()),
             JTokenType.String => int.TryParse(token.Value<string>(), out var result)
@@ -58,7 +58,7 @@ public static class JsonHelper
     /// <param name="token"> JSON source </param>
     /// <param name="ignoreTypeMismatch"> Skip values that can't be parsed, otherwise return empty array </param>
     public static int[] TryGetIntArray(this JToken token, bool ignoreTypeMismatch = true)
-        => token.Type switch
+        => token?.Type switch
         {
             JTokenType.Array => !ignoreTypeMismatch
                                 && token.Any(item => item.Type is not (JTokenType.Integer or JTokenType.Float or JTokenType.String))
@@ -82,7 +82,7 @@ public static class JsonHelper
     /// <summary> Try get <see cref="bool" /> value </summary>
     /// <param name="token"> JSON source </param>
     public static Maybe<bool> TryGetBool(this JToken token)
-        => token.Type switch
+        => token?.Type switch
         {
             JTokenType.Boolean => Maybe.Value(token.Value<bool>()),
             JTokenType.Integer or JTokenType.Float => Maybe.Value(token.Value<float>() > 0),
@@ -99,4 +99,35 @@ public static class JsonHelper
     /// <param name="paramName"> Property name </param>
     public static Maybe<bool> TryGetBool(this JToken token, string paramName)
         => token[paramName]?.TryGetBool() ?? Maybe.None<bool>();
+
+    /// <summary> Try get <see cref="bool" /> value </summary>
+    /// <param name="token"> JSON source </param>
+    public static bool GetBoolOrFalse(this JToken token)
+        => token.TryGetBool().ValueOrDefault(false);
+
+    /// <summary> Try get <see cref="bool" /> value </summary>
+    /// <param name="token"> JSON source </param>
+    /// <param name="paramName"> Property name </param>
+    public static bool GetBoolOrFalse(this JToken token, string paramName)
+        => token[paramName]?.GetBoolOrFalse() ?? false;
+
+    /// <summary> Try get <see cref="DateTime" /> value </summary>
+    /// <param name="token"> JSON source </param>
+    public static Maybe<DateTime> TryGetDateTime(this JToken token)
+    {
+        try
+        {
+            return Maybe.Value(token.Value<DateTime>());
+        }
+        catch (Exception)
+        {
+            return Maybe.None<DateTime>();
+        }
+    }
+
+    /// <summary> Try get <see cref="DateTime" /> value </summary>
+    /// <param name="token"> JSON source </param>
+    /// <param name="paramName"> Property name </param>
+    public static Maybe<DateTime> TryGetDateTime(this JToken token, string paramName)
+        => token[paramName]?.TryGetDateTime() ?? Maybe.None<DateTime>();
 }
