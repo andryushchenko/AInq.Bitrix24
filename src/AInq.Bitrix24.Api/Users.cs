82d9c9ce8aef8598bec1a9fa9ab9ab60aef7cdb8
FreddyFox1 <svchaykun@mail.ru>
2021-11-05 15:36:06 +0300

    Added API method to get users list of Bitrix24 application

---
+++ src/AInq.Bitrix24.Api/Users.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace AInq.Bitrix24
+{
+    public class Users
+    {
+        public ICollection<UserModel>? List { get; private set; }
+
+        private readonly IBitrix24Client client;
+        private const string method = "/user.get.json";
+
+        public Users(IBitrix24Client client)
+        {
+            this.client = client;
+            GetUsers();
+        }
+
+        private async void GetUsers()
+        {
+            var data = await client.GetAsync(method);
+            if (data != null)
+            {
+                try
+                {
+                    List = JsonConvert.DeserializeObject<UserModel.Root>(data.Content);
+                }
+                catch (Exception ex)
+                {
+                    throw new Bitrix24CallException(method, "Error parsing response", ex);
+                }
+            }
+        }
+
+
+    }
+}
